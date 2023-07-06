75f2f0ba1e55375cc9b2d302f615189d83fa8816
areignel <48579497+areignel@users.noreply.github.com>
2023-07-06 15:20:28 +0100

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 
-"""This module provides a function for adding two floats."""
+'''
+    Basic annotations for variables.
+'''
+
 
 def add(a: float, b: float) -> float:
-    """
-    Add two floats and return their sum.
-
-    Args:
-        a: The first float.
-        b: The second float.
-
-    Returns:
-        The sum of the two floats as a float.
-    """
+    '''
+        Returns sum of two floats.
+    '''
     return a + b
